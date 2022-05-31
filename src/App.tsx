6f924cc1d0246d867ff28a043db9b1e162c94a9a
basuabhirup <basu.abhirup@yahoo.com>
2022-05-31 22:05:27 +0530

    Add Mascot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
-// import Mascot from './components/mascot/Mascot'
+import Mascot from './components/mascot/Mascot'
 import Footer from './components/footer/Footer'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <Header />
-      {/* <Mascot /> */}
+      <Mascot />
       <Footer />
     </div>
   )
--- src/components/mascot/Mascot.tsx
+++ src/components/mascot/Mascot.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import './Mascot.css'
+import Mascot0 from '../../assets/svg/Mascot0.svg'
+// import Mascot1 from '../../assets/svg/Mascot1.svg'
+// import Mascot2 from '../../assets/svg/Mascot2.svg'
 
 const Mascot: React.FC = () => {
-  return <div>Mascot</div>
+  return (
+    <>
+      <img className='mascot' src={Mascot0} alt='Neutral Face' />
+    </>
+  )
 }
 
 export default Mascot
