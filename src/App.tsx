8e5092472cbf37e50ab0de3307abd1adea678715
basuabhirup <basu.abhirup27@gmail.com>
2023-08-13 21:37:42 +0530

    Validate the  selected option & reflect it in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import React from 'react'
-import './App.css'
-import Header from './components/header/Header'
-import Mascot from './components/mascot/Mascot'
-import Options from './components/options/Options'
-import Footer from './components/footer/Footer'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Mascot from "./components/mascot/Mascot";
+import Options from "./components/options/Options";
+import Footer from "./components/footer/Footer";
+import { IPair } from "./utils/interfaces";
 
 const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [selectionStatus, setSelectionStatus] = useState<boolean | null>(null);
+  const [pair] = useState<IPair>({
+    pair_id: 0,
+    options: ["Chicken", "Egg"],
+    correct_option: "Egg",
+    description:
+      "The age-old question: Which came first, the chicken or the egg? While chickens are descendants of birds that were not quite chickens, genetic mutations occured in their offspring, leading to the first bird that we would call a chicken. Therefore, the first true chicken must have hatched from an egg laid by a bird that was very close to a chicken but not quite there.",
+  });
+
+  useEffect(() => {
+    if(selectionStatus === true || selectionStatus === false) {
+      setTimeout(() => {
+        alert('Display Modal')
+        // Code Block to display Modal
+      }, 1500)
+    }
+  }, [selectionStatus])
+  
   return (
-    <div className='App'>
-      <Header />
-      <Mascot />
-      <Options />
+    <div className="App">
+      <Header pair={pair}/>
+      <Mascot isLoading={isLoading} selectionStatus={selectionStatus} />
+      <Options
+        pair={pair}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+        setSelectionStatus={setSelectionStatus}
+      />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/mascot/Mascot.tsx
+++ src/components/mascot/Mascot.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import './Mascot.css'
-import Mascot0 from '../../assets/svg/Mascot0.svg'
-// import Mascot1 from '../../assets/svg/Mascot1.svg'
-// import Mascot2 from '../../assets/svg/Mascot2.svg'
+import React from "react";
+import "./Mascot.css";
+import Mascot0 from "../../assets/svg/Mascot0.svg";
+import Mascot1 from "../../assets/svg/Mascot1.svg";
+import Mascot2 from "../../assets/svg/Mascot2.svg";
 
-const Mascot: React.FC = () => {
-  return (
-    <>
-      <img className='mascot' src={Mascot0} alt='Neutral Face' />
-    </>
-  )
+interface IProps {
+  isLoading: boolean
+  selectionStatus: boolean | null;
 }
 
-export default Mascot
+const Mascot: React.FC<IProps> = ({ isLoading, selectionStatus }) => {
+  return (
+    <div className="mascot-container">
+      {selectionStatus === null && (
+        <img className="mascot" src={Mascot0} alt="Neutral Face" />
+      )}
+      {selectionStatus === true && (
+        <img className="mascot" src={Mascot1} alt="Happy Face" />
+      )}
+      {selectionStatus === false && (
+        <img className="mascot" src={Mascot2} alt="Sad Face" />
+      )}
+    </div>
+  );
+};
+
+export default Mascot;
