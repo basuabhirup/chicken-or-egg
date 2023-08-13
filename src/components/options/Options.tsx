7992f95d304abdd5658db1d53cd1b390bbe46aa0
basuabhirup <basu.abhirup27@gmail.com>
2023-08-13 19:03:36 +0530

    Finalize the basic responsive UI layout

--- src/components/options/Options.tsx
+++ src/components/options/Options.tsx
@@ -1,17 +1,20 @@
-import React from "react"
-import './Options.css'
+import React, { useState } from "react";
+import "./Options.css";
 
 const Options: React.FC = () => {
+  const [options] = useState<String[]>(["Chicken", "Egg"]);
   return (
     <>
-      <section className='options'>
-        <button className='option'>Chicken</button>
-        <p className='conjunction'>or</p>
-        <button className='option'>Egg</button>
+      <section className="options">
+        <button className="option">{options[0]}</button>
+        <p className="conjunction">or</p>
+        <button className="option">{options[1]}</button>
       </section>
-      <p className='info-text'>Click to start the game, and end the lifelong dilemma</p>
+      <p className="info-text">
+        Click to start the game, and end the lifelong dilemma
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default Options
+export default Options;
