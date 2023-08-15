37fdc808f0cbf28057cadfc5823f7d02270cd89d
basuabhirup <basu.abhirup27@gmail.com>
2023-08-15 23:58:22 +0530

    Implement Googly Eyes

--- src/components/mascot/Mascot.tsx
+++ src/components/mascot/Mascot.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Mascot.css";
-import Mascot0 from "../../assets/svg/Mascot0.svg";
-import Mascot1 from "../../assets/svg/Mascot1.svg";
-import Mascot2 from "../../assets/svg/Mascot2.svg";
+import { MascotNeutral } from "../../assets/svg/MascotNeutral";
+import MascotHappy from "../../assets/svg/Mascot1.svg";
+import MascotSad from "../../assets/svg/Mascot2.svg";
 import Loader_Mascot from "../../assets/Loader_Mascot.gif";
 
 interface IProps {
@@ -11,6 +11,67 @@ interface IProps {
 }
 
 const Mascot: React.FC<IProps> = ({ isFetching, selectionStatus }) => {
+  const applyGooglyEyes = () => {
+    const eyeball_left = document.querySelector(".eyeball_left");
+    const pupil_left = document.querySelector(".pupil_left");
+    const eyeball_right = document.querySelector(".eyeball_right");
+    const pupil_right = document.querySelector(".pupil_right");
+
+    const pupils = [
+      { pupil: pupil_left, eyeball: eyeball_left },
+      { pupil: pupil_right, eyeball: eyeball_right },
+    ];
+
+    const maxX = 3; // Maximum horizontal movement
+    const maxY = 2; // Maximum vertical movement
+
+    pupils.forEach(({ pupil, eyeball }: any) => {
+      (pupil as any).style.transition = "transform 0.2s ease-out"; // Add CSS transition
+    });
+
+    const handleMouseMove = (e: any) => {
+      pupils.forEach(({ pupil, eyeball }) => {
+        const { left, top, width, height } = (
+          eyeball as any
+        ).getBoundingClientRect();
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+
+        const x = e.clientX - centerX;
+        const y = e.clientY - centerY;
+
+        const moveX = Math.min(Math.max((x / width) * maxX, -maxX), maxX);
+        const moveY = Math.min(Math.max((y / height) * maxY, -maxY), maxY);
+
+        (pupil as any).style.transform = `translate(${moveX}px, ${moveY}px)`;
+      });
+    };
+
+    return handleMouseMove;
+  };
+
+  // Apply Side Effect to apply Googly Eyes
+  useEffect(() => {
+    const handleMouseMove = applyGooglyEyes();
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    let handleMouseMove: (e: any) => void;
+    if (!isFetching) {
+      handleMouseMove = applyGooglyEyes();
+      document.addEventListener("mousemove", handleMouseMove);
+    }
+    return () => {
+      if (!isFetching) {
+        document.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, [isFetching]);
+
   if (!!isFetching) {
     return (
       <div className="mascot-container">
@@ -27,13 +88,16 @@ const Mascot: React.FC<IProps> = ({ isFetching, selectionStatus }) => {
   return (
     <div className="mascot-container">
       {selectionStatus === null && (
-        <img className="mascot" src={Mascot0} alt="Neutral Face" />
+        <div
+          className="mascot"
+          dangerouslySetInnerHTML={{ __html: MascotNeutral }}
+        />
       )}
       {selectionStatus === true && (
-        <img className="mascot" src={Mascot1} alt="Happy Face" />
+        <img className="mascot" src={MascotHappy} alt="Happy Face" />
       )}
       {selectionStatus === false && (
-        <img className="mascot" src={Mascot2} alt="Sad Face" />
+        <img className="mascot" src={MascotSad} alt="Sad Face" />
       )}
     </div>
   );
