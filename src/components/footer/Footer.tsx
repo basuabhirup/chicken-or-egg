b049989ff34507722bff10650c15a1be42bd583b
anushabanerjee <rairaianusha@gmail.com>
2023-08-16 22:50:18 +0530

    Added href links to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,8 +4,8 @@ import './Footer.css'
 const Footer: React.FC = () => {
   return (
     <footer>
-      <div>Made with curiosity by Anusha {`&`} Abhirup</div>
-      <div>Copyright {new Date().getFullYear()} © Chicken Or Egg</div>
+      <div>Made with curiosity by <a className="link" href="https://www.behance.net/anushabanerjee" target="_blank" rel="noopener noreferrer">Anusha</a> {`&`} <a className="link" href="https://github.com/basuabhirup" target="_blank" rel="noopener noreferrer">Abhirup</a> </div>
+      <div>Copyright {new Date().getFullYear()} © <a className="link" href="https://github.com/basuabhirup/chicken-or-egg" target="_blank" rel="noopener noreferrer">Chicken Or Egg </a></div>
     </footer>
   )
 }
